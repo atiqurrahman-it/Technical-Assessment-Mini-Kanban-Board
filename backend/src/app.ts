c3da74128c8e7bf9ef311aeb724aabce05552084
atiqurrahman-it <atiqurrahman.cse2552@gmail.com>
2026-09-06 04:52:18 +0600

    feat: add Docker configuration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors';
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
+import httpStatus from 'http-status';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFoundHandler from './app/middlewares/notFoundHandler';
 import router from './app/routes/route';
@@ -9,6 +10,14 @@ const app: Application = express();
 
 app.use(cors({ origin: config.cors_origin, credentials: true }));
 app.use(express.json());
+
+// home page
+app.get('/', (_req: Request, res: Response) => {
+  res.status(httpStatus.OK).json({
+    success: true,
+    message: 'Express server is running!',
+  });
+});
 
 app.get('/health', (_req, res) => res.json({ status: 'ok' }));
 
